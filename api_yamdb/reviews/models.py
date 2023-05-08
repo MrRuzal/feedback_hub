a45479f3423c9ffd6191cf98239c401b37fb8cdf
Ruzal Zakirov <email.com>
2023-05-08 16:43:17 +0300

    added model User for reviews app

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        USER = 'user', 'пользователь'
+        MODERATOR = 'moderator', 'модератор'
+        ADMIN = 'admin', 'администратор'
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=10, choices=Role.choices, verbose_name='Роли'
+    )
