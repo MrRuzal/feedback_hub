e4437d38d5d4812e8c40c0d4c2d60a996fcb8482
Андрей Габриэлис <andrei.gabrielis@yandex.ru>
2023-05-09 11:25:10 +0300

    first_title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class User(AbstractUser):
@@ -17,4 +20,55 @@ class User(AbstractUser):
         choices=Role.choices,
         default=Role.USER,
         verbose_name='Роли',
+    )
+
+
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[-a-zA-Z0-9_]+$',
+                code=Response(status=status.HTTP_400_BAD_REQUEST)
+            )
+        ]
+    )
+
+
+class Genres(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        unique=True,
+        validators=[
+            RegexValidator(regex=r'^[-a-zA-Z0-9_]+$')
+        ]
+    )
+
+
+class Titles(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения',
+    )
+    pub_date = models.IntegerField()
+    description = models.TextField(
+        verbose_name='Описание произведения'
+    )
+    categories = models.ForeignKey(
+        Categories,
+        related_name='title',
+        on_delete=models.CASCADE,
+    )
+    genres = models.ForeignKey(
+        Genres,
+        related_name='title',
+        on_delete=models.CASCADE
     )
