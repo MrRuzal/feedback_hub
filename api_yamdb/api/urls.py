65cfc0f6cdeb25e2e8178159267edd702e119425
Шамиль Эбзеев <shamilebzeev@hotmail.com>
2023-05-11 16:54:16 +0300

    some changes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,11 +8,12 @@ router_v1 = DefaultRouter()
 router_v1.register('titles', TitleVewSet)
 router_v1.register('categories', CategoriesViewSet)
 router_v1.register('genres', GenresViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewVeiewSet,
+                   basename='review')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='commments')
-router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewVeiewSet,
-                   basename='review')
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
