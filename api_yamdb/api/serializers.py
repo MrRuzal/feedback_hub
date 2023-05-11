7e8ac36a1911a48523e3a9c3db4719de493575fa
Андрей Габриэлис <andrei.gabrielis@yandex.ru>
2023-05-12 00:49:35 +0300

    fix

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -120,7 +120,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
         model = Review
 
     def validate(self, attrs):
